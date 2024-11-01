8dbd5451a9b19622ad4536598b3a8aee74285fe5
ek-sarath <sarath.e@edstem.com>
2024-11-01 12:58:50 +0530

    fixed header/page.tsx for search

--- src/app/header/page.tsx
+++ src/app/header/page.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, ChangeEvent } from "react";
 import Image from "next/image";
 import Logo from "../../../public/logo.png";
 
@@ -9,9 +9,10 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ onSearch }) => {
   const [query, setQuery] = useState("");
 
-  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    onSearch(query);
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newQuery = e.target.value;
+    setQuery(newQuery);
+    onSearch(newQuery);
   };
 
   return (
@@ -19,18 +20,18 @@ const Header: React.FC<HeaderProps> = ({ onSearch }) => {
         <div>
         <Image src={Logo} width={50} height={50} alt="Logo" />
       <h1 className="text-3xl text-blue-700 font-bold ml-7">Recipe App</h1></div>
-      <form onSubmit={handleSearch} className="mt-2 mr-6 gap-2 flex items-center">
+      <div className="mt-2 mr-6 gap-2 flex items-center">
         <input
           type="text"
           value={query}
-          onChange={(e) => setQuery(e.target.value)}
+          onChange={handleChange}
           placeholder="Search Recipes..."
           className="p-2 w-56 border-2 border-gray-300 rounded text-sm text-gray-700"
         />
         <button type="submit" className="ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 rounded">
           Search
         </button>
-      </form>
+      </div>
     </div>
   );
 };
